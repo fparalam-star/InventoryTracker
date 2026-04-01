05f340d7586ac6a866107d2a264d48f0e7c3be0f
fparalam-star <fparalam@gmail.com>
2026-04-01 21:18:10 +0200

    Initialize project and add Vercel config

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { registerRoutes } from "../server/routes";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+let initialized = false;
+
+export default async function handler(req: any, res: any) {
+  if (!initialized) {
+    await registerRoutes(app);
+    initialized = true;
+  }
+  return app(req, res);
+}
